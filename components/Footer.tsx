5b5b658a005d2f73e4daaf635e1eb139b824e58d
Stark-Priver <priverdeprince@yahoo.com>
2026-03-02 10:37:52 +0300

    Added the Youtube Icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -65,6 +65,19 @@ const Footer = () => (
                 <path d='M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-2.88 2.5 2.89 2.89 0 01-2.89-2.89 2.89 2.89 0 012.89-2.89c.28 0 .54.04.79.1v-3.5a6.37 6.37 0 00-.79-.05A6.34 6.34 0 003.15 15.2a6.34 6.34 0 0010.86 4.48 6.3 6.3 0 001.88-4.48V8.74a8.26 8.26 0 004.84 1.56V6.85a4.84 4.84 0 01-1.14-.16z'/>
               </svg>
             </Link>
+
+            {/* YouTube */}
+            <Link
+              href='https://youtube.com/@bingwamagari'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='group w-10 h-10 rounded-xl bg-gray-800 flex items-center justify-center hover:bg-[#FF0000] hover:scale-110 hover:shadow-[0_0_20px_rgba(255,0,0,0.4)] transition-all duration-300'
+              aria-label='YouTube'
+            >
+              <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24' fill='currentColor' className='text-gray-400 group-hover:text-white transition-colors'>
+                <path d='M23.498 6.186a3.016 3.016 0 00-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 00.502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 002.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 002.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z'/>
+              </svg>
+            </Link>
           </div>
         </div>
 
