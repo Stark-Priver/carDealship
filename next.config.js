a0cac045d81ed23c4c3510c2fc2bc25197f34023
Stark-Priver <priverdeprince@yahoo.com>
2026-03-13 12:06:11 +0300

    update: next.config.js [2026-03-13 12:06:11]

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["cdn.imagin.studio"]
-    }
-}
+        domains: ["cdn.imagin.studio"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**.supabase.co",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
